026a87bbdff0a80edaa22a0ebcbe8c4eeb4c38ca
Colin Reeder <vpzomtrrfrt@gmail.com>
2014-05-27 11:09:16 -0600

    Charlie now eats invasive plants, and doesn't eat himself.

--- java/net/reederhome/colin/theandrewmod/EntityCharlie.java
+++ java/net/reederhome/colin/theandrewmod/EntityCharlie.java
@@ -6,7 +6,10 @@ import java.util.Random;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 public class EntityCharlie extends EntityChicken {
@@ -20,14 +23,30 @@ public class EntityCharlie extends EntityChicken {
 			List<EntityLivingBase> l = worldObj.getEntitiesWithinAABB(EntityLivingBase.class, AxisAlignedBB.getBoundingBox(posX-0.5, posY-0.5, posZ-0.5, posX+1, posY+1, posZ+1));
 			if(l.size()>0) {
 				EntityLivingBase thing = l.get(new Random().nextInt(l.size()));
-				thing.attackEntityFrom(TheAndrewMod.deathByCharlie, 20f);
+				if(thing!=this) {
+					thing.attackEntityFrom(TheAndrewMod.deathByCharlie, 20f);
+				}
 			}
 		}
+		else if(Math.random()<0.01) {
+			if(!eatPlant(posX,posY,posZ)) if(!eatPlant(posX+1,posY,posZ)) if(!eatPlant(posX,posZ,posZ+1)) if(!eatPlant(posX-1, posY, posZ)) eatPlant(posX, posY, posZ-1);
+		}
+	}
+	public boolean eatPlant(double posX, double posY, double posZ) {
+		if(worldObj.getBlock((int)posX, (int)posY, (int)posZ).equals(TheAndrewMod.invasivePlant)) {
+			worldObj.setBlockToAir((int)posX, (int)posY, (int)posZ);
+			this.heal(1);
+			return true;
+		}
+		return false;
 	}
 	public EntityCharlie createChild(EntityAgeable a) {
 		return new EntityCharlie(worldObj);
 	}
 	public String getHurtSound() {
 		return TheAndrewMod.MODID+":mob.charlie.scarp";
+	}
+	public void onDeath(DamageSource source) {
+		System.out.println(source.damageType);
 	}
 }
