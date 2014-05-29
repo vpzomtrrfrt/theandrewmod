262e35efceda9acb24c1a98f5a73b3e9c9340d06
Colin Reeder <vpzomtrrfrt@gmail.com>
2014-05-29 06:35:57 -0600

    v1.2.1!

--- java/net/reederhome/colin/theandrewmod/TheAndrewMod.java
+++ java/net/reederhome/colin/theandrewmod/TheAndrewMod.java
@@ -52,7 +52,7 @@ import cpw.mods.fml.common.registry.VillagerRegistry.IVillageTradeHandler;
 public class TheAndrewMod {
 
 	public static final String MODID = "theandrewmod";
-	public static final String VERSION = "1.2.0";
+	public static final String VERSION = "1.2.1";
 	static ItemArmor.ArmorMaterial glassBottleArmorMaterial = EnumHelper.addArmorMaterial("glassBottle", 8, new int[]{1, 3, 3, 1}, 16);
 	static DamageSource deathBy789 = new DamageSource("theandrewmod.deathBy789");
 	static DamageSource deathByPotatoLiver = new DamageSource("theandrewmod.deathByPotatoLiver");
