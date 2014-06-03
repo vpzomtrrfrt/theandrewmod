be235b6d6e112f4133344dd78bd43262aa1e620b
Colin Reeder <vpzomtrrfrt@gmail.com>
2014-06-03 08:41:02 -0600

    Burning butter dust gives effects to living entities nearby

--- java/net/reederhome/colin/theandrewmod/ItemButterDust.java
+++ java/net/reederhome/colin/theandrewmod/ItemButterDust.java
@@ -1,6 +1,8 @@
 package net.reederhome.colin.theandrewmod;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
@@ -20,5 +22,16 @@ public class ItemButterDust extends ItemFood {
 		player.addPotionEffect(new PotionEffect(Potion.confusion.getId(), 600, 0));
 		player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 600, 95));
 		player.addPotionEffect(new PotionEffect(Potion.digSlowdown.getId(), 600, 1));
+	}
+	public boolean hasCustomEntity(ItemStack stack) {
+		return true;
+	}
+	public Entity createEntity(World world, Entity location, ItemStack itemstack) {
+		EntityItem tr = new EntityItemButterDust(world);
+		tr.setEntityItemStack(itemstack);
+		tr.setLocationAndAngles(location.posX, location.posY, location.posZ, location.rotationPitch, location.rotationYaw);
+		tr.setVelocity(location.motionX, location.motionY, location.motionZ);
+		tr.delayBeforeCanPickup=((EntityItem)location).delayBeforeCanPickup;
+		return tr;
 	}
 }
