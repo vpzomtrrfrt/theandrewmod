0fdd9012d33b213c4d683f93525f029c45714243
Colin Reeder <vpzomtrrfrt@gmail.com>
2014-10-25 13:28:56 -0600

    Throwable butter cluster!

--- java/net/reederhome/colin/theandrewmod/item/ItemButterCluster.java
+++ java/net/reederhome/colin/theandrewmod/item/ItemButterCluster.java
@@ -1,9 +1,13 @@
 package net.reederhome.colin.theandrewmod.item;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityPotion;
+import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 import net.reederhome.colin.theandrewmod.TheAndrewMod;
@@ -19,7 +23,28 @@ public class ItemButterCluster extends Item {
 	}
 	
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer p) {
-		p.setItemInUse(stack, getMaxItemUseDuration(stack));
+		if(p.isSneaking()) {
+			if(!world.isRemote) {
+				ItemStack pst = new ItemStack(Items.potionitem);
+				NBTTagCompound tag = new NBTTagCompound();
+				NBTTagList ptl = new NBTTagList();
+				NBTTagCompound ptg = new NBTTagCompound();
+				ptg.setByte("Id", (byte) TheAndrewMod.clusterPotion.id);
+				ptg.setByte("Amplifier", (byte)0);
+				ptg.setInteger("Duration", 2000);
+				ptl.appendTag(ptg);
+				tag.setTag("CustomPotionEffects", ptl);
+				pst.setTagCompound(tag);
+				EntityPotion pot = new EntityPotion(world, p, pst);
+				world.spawnEntityInWorld(pot);
+			}
+			if(!p.capabilities.isCreativeMode) {
+				return new ItemStack(ItemsAndrew.butterShellEmpty);
+			}
+		}
+		else {
+			p.setItemInUse(stack, getMaxItemUseDuration(stack));
+		}
 		return stack;
 	}
 	
