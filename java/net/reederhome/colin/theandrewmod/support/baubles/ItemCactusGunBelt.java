7ad318d3fc0cbeea86fa51800b9c23f1e682c567
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-04-25 15:30:55 -0600

    More cactus gun belt stuff

--- java/net/reederhome/colin/theandrewmod/support/baubles/ItemCactusGunBelt.java
+++ java/net/reederhome/colin/theandrewmod/support/baubles/ItemCactusGunBelt.java
@@ -4,11 +4,19 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.reederhome.colin.theandrewmod.TheAndrewMod;
 import baubles.api.BaubleType;
 import baubles.api.IBauble;
 
 public class ItemCactusGunBelt extends Item implements IBauble {
 
+	public ItemCactusGunBelt() {
+		super();
+		setTextureName(TheAndrewMod.MODID+":cactusGunBelt");
+		setCreativeTab(TheAndrewMod.tabAndrew);
+		setUnlocalizedName("cactusGunBelt");
+	}
+	
 	@Override
 	public BaubleType getBaubleType(ItemStack itemstack) {
 		return BaubleType.BELT;
@@ -33,7 +41,4 @@ public class ItemCactusGunBelt extends Item implements IBauble {
 		return true;
 	}
 
-	public boolean getIsRepairable(ItemStack stack, ItemStack it) {
-		return it.getItem().equals(Blocks.cactus);
-	}
 }
