9a45e3a8f58b0e21c58fbb08a4846d621f7431a1
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-05-04 08:30:54 -0600

    Bump version

--- java/net/reederhome/colin/theandrewmod/TheAndrewMod.java
+++ java/net/reederhome/colin/theandrewmod/TheAndrewMod.java
@@ -124,7 +124,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class TheAndrewMod {
 
 	public static final String MODID = "theandrewmod";
-	public static final String VERSION = "1.16.0pre";
+	public static final String VERSION = "1.16.0";
 	public static final String NAME = "The Andrew Mod";
 	public static CreativeTabs tabAndrew = new CreativeTabs(CreativeTabs.getNextID(), "theandrewmod") {	
 		@Override
