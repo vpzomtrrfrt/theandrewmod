d7f99d2a7b888b370d56f73bfcbb1ecdd8591590
Colin Reeder <vpzomtrrfrt@gmail.com>
2015-05-26 17:23:24 -0600

    Explosion tweaks for Liquid Gunpowder

--- java/net/reederhome/colin/theandrewmod/block/BlockLiquidGunpowder.java
+++ java/net/reederhome/colin/theandrewmod/block/BlockLiquidGunpowder.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidClassic;
 import net.reederhome.colin.theandrewmod.TheAndrewMod;
@@ -27,16 +28,24 @@ public class BlockLiquidGunpowder extends BlockFluidClassic {
 		return meta==0?blockIcon:flow;
 	}
 	
+	public void doExplode(World w, int x, int y, int z) {
+		w.createExplosion(null, x, y, z, 2, true);
+	}
+	
 	public boolean displaceIfPossible(World w, int x, int y, int z) {
 		Block b = w.getBlock(x, y, z);
 		boolean isLava = b.getMaterial().equals(Material.lava);
 		if(b.equals(Blocks.torch) || b.equals(Blocks.fire) || isLava) {
-			w.createExplosion(null, x, y, z, 1, true);
+			doExplode(w, x, y, z);
 			if(!isLava) {
 				return true;
 			}
 		}
 		return super.displaceIfPossible(w, x, y, z);
+	}
+	
+	public void onBlockDestroyedByExplosion(World w, int x, int y, int z, Explosion p_149723_5_) {
+		doExplode(w, x, y, z);
 	}
 	
 }
